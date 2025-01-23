a532f4d38e81592fb4d4fbb3c0c812cdcac1a0e7
Rooney kalumba <22ki129@esisalama.org>
2025-01-23 11:39:19 +0200

    Fix calculate

--- app/src/main/java/com/example/pourboir/MainActivity.kt
+++ app/src/main/java/com/example/pourboir/MainActivity.kt
@@ -16,15 +16,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pourboir.ui.theme.PourboirTheme
@@ -47,6 +53,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -62,9 +71,9 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(modifier =Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(value=amountInput, onValueChange = {amountInput=it},modifier =Modifier.padding(bottom = 32.dp).fillMaxWidth())
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -83,11 +92,17 @@ fun TipTimePreviw(){
     TipTimeLayout()
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier){
+fun EditNumberField(modifier: Modifier = Modifier,value:String,onValueChange:(String)->Unit){
     TextField(
-        value = "",
-        onValueChange = {},
-        modifier = modifier
+        label = {
+            Text(stringResource(R.string.bill_amount))
+        },
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
