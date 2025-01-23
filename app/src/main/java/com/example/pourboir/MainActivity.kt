aa11315640c23244db6daa00c7615617383575e2
Rooney kalumba <22ki129@esisalama.org>
2025-01-23 12:00:03 +0200

    Fix calculate tip

--- app/src/main/java/com/example/pourboir/MainActivity.kt
+++ app/src/main/java/com/example/pourboir/MainActivity.kt
@@ -80,7 +80,6 @@ fun TipTimeLayout() {
     }
 }
 
-
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
